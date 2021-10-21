def46070cedc59c6b04fef1ac1fcfe9a8d8f61be
Larry Hatcer <actswolf@gmail.com>
2021-10-20 20:07:07 -0500

    Got comments to display. Will still need to pass in videoId of a search.

--- src/components/Comment/comment.jsx
+++ src/components/Comment/comment.jsx
@@ -3,15 +3,26 @@ import axios from 'axios';
 
 
 const Comment = (props) => {
-    const [videoId, setVideoId] = useState({});
-
+    const [comments, setComments] = useState({});
+    
     useEffect(() => {
-        axios.get('http://localhost:5000/api/comments')
-            .then(response => setVideoId(response.data[props.index])
-        )}, [videoId, props.index]);
-
-    // return (
-
+        axios.get('http://localhost:5000/api/comments/M7lc1UVf-VE')
+            .then(response => setComments(response.data))
+            }, []);
+    
+        return (
+        <div>
+            <tbody>
+            {comments && comments.map((comment) => {
+                return(
+                    <tr key={comment.id} >
+                        <td>{comment.text}</td>
+                    </tr>
+                )
+            })}
+            </tbody>
+        </div>
+    )
 }
 
 export default Comment;
