dfca108c7585873a83deb9a06ab3e16a1f9e24ac
amandadomer <amdomer@icloud.com>
2021-10-20 20:08:48 -0500

    merge

--- src/components/Comment/comment.jsx
+++ src/components/Comment/comment.jsx
@@ -1,17 +1,17 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
-
 const Comment = (props) => {
-    const [videoId, setVideoId] = useState({});
+    const [comments, setComments] = useState({});
 
     useEffect(() => {
-        axios.get('http://localhost:5000/api/comments')
-            .then(response => setVideoId(response.data[props.index])
-        )}, [videoId, props.index]);
+        axios.get('http://localhost:5000/api/comments/videoId')
+            .then(response => setComments(response.data)
+        ), [videoId]);
 
-    // return (
-
-}
+    return (
+        <div><h1>{comments.map()}</h1></div>
+    )
+}    
 
 export default Comment;
