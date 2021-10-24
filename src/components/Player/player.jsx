dcd49655e0789955bfddaf35688572bd7b5885e1
Larry Hatcer <actswolf@gmail.com>
2021-10-24 09:17:08 -0500

    Fixed code where everything is showing again. Fixed api key crashing.

--- src/components/Player/player.jsx
+++ src/components/Player/player.jsx
@@ -4,7 +4,7 @@ import React from 'react';
 const Player = (props) => {
     return (
         <iframe id="ytplayer" type="text/html" width="640" height="360"
-        src={`https://www.youtube.com/embed/YQHsXMglC9A?autoplay=1&origin=http://example.com`}
+        src={`https://www.youtube.com/embed/${props.videoId}?autoplay=1&origin=http://example.com`}
         frameBorder="0" title="whatIsPlaying"></iframe>
  
     )
--- src/components/app.jsx
+++ src/components/app.jsx
@@ -4,13 +4,13 @@ import TitleBar from './TitleBar/titleBar';
 import Player from './Player/player';
 import Comment from './Comment/comment';
 import RelatedVideos from './RelatedVideos/relatedVideos';
-import AddNewComment from './AddNewComment/addNewComment';
+import AddNewComment from './AddNewComment/addNewComment'
 import axios from 'axios';
 
 const App = () =>{
     const [videoId, setVideoId] = useState ("1EQg9kV6f98");
-    const [comment, setComment] = useState ("")
-    const [reply, setReply] = useState (null);
+    // const [comment, setComment] = useState ("")
+    // const [reply, setReply] = useState (null);
 
     useEffect(() => {
         axios.get (`https://www.googleapis.com/youtube/v3/search?q=${videoId}&key=AIzaSyCv9Tmp3CSZpF2sPmQOB3tQVY8BqywQs6c`)
@@ -25,9 +25,10 @@ const App = () =>{
             <TitleBar />
             <SearchBar setVideoId = {setVideoId}/>
             <Player videoId = {videoId}/>
-            <AddNewComment videoId = {videoId}/>
+            <AddNewComment videoId = {videoId} />
             <Comment videoId = {videoId}/>
             <RelatedVideos videoId = {videoId}/>
+            
         </div>
     )
 }
